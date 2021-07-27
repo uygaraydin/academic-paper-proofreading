ce2c6215ed58d581c2324fcadad549e2563a0c08
uygar aydin <hi@uygaraydin.com>
2021-07-27 11:55:24 +0300

    academic user was removed from the class diagram and classes was updated as these changes

--- index.js
+++ index.js
@@ -1,143 +1,156 @@
 class User {
-    constructor(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents) {
-        this.name = name
-        this.surname = surname
-        this.email = email
-        this.phone = phone
-        this.department = department
-        this.title = title
-        this.role = role
-        this._identificationNumber = identificationNumber
-        this._institutionalNumber = institutionalNumber
-        this.documents = documents
-    }
+  #identificationNumber = null
+  #institutionalNumber = null
+  constructor(name, surname, email, phone, department, title, identificationNumber, institutionalNumber, documents, role) {
+    this.name = name
+    this.surname = surname
+    this.email = email
+    this.phone = phone
+    this.department = department
+    this.title = title
+    this.#identificationNumber = identificationNumber
+    this.#institutionalNumber = institutionalNumber
+    this.documents = documents
+    this.role = role
+  }
 
-    login() {
+  login() {
 
-    }
+  }
 
-    getStatistics(){
+  getStatistics() {
 
-    }
+  }
 
-    controlUserRecord(user) {
+  #controlUserRecord(user) {
+    console.log(user)
+  }
 
-    }
+  #add(user) {
 
-    add(user) {
+  }
 
-    }
-}
-
-class Academic extends User {
-    constructor(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents = []){
-        super(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents)
-    }
-
-    upload(document) {
-        return document
-    }
+  upload(document) {
+    return document
+  }
 }
 
 class Translator extends User {
-    constructor(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents = []){
-        super(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents)
-    }
-    
-    upload(document) {
-        return document
-    }
+  constructor(name, surname, email, phone, department, title, identificationNumber, institutionalNumber, documents = [], role) {
+    super(name, surname, email, phone, department, title, identificationNumber, institutionalNumber, documents, role)
+  }
 
-    addAsManually(user) {
-        return user
-    }
+  upload(document) {
+    return document
+  }
 
-    removeManuallyAdded(user) {
-        return user
-    }
+  addAsManually(user) {
+    return user
+  }
+
+  removeManuallyAdded(user) {
+    return user
+  }
 }
 
 class Admin extends User {
-    constructor(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents = []){
-        super(name, surname, email, phone, department, title, role, identificationNumber, institutionalNumber, documents)
-    }
-    
-    delete(user) {
-        return document
-    }
+  constructor(name, surname, email, phone, department, title, identificationNumber, institutionalNumber, documents = [], role) {
+    super(name, surname, email, phone, department, title, identificationNumber, institutionalNumber, documents, role)
+  }
 
-    changeRole(user) {
-        return user
-    }
+  delete(user) {
+    return document
+  }
 
-    edit(staticPage) {
-        return staticPage
-    }
+  changeRole(user) {
+    return user
+  }
 
-    add(permission) {
-        return permission
-    }
+  edit(staticPage) {
+    return staticPage
+  }
 
-    delete(permission) {
-        return permission
-    }
+  add(permission) {
+    return permission
+  }
 
-    updatePermission(permission) {
-        return permission
-    }
+  delete(permission) {
+    return permission
+  }
 
-    updateRole(role) {
-        return role
-    }
+  updatePermission(permission) {
+    return permission
+  }
 
-    find(searchKey) {
-        const results = []
-        return results
-    }
+  updateRole(role) {
+    return role
+  }
 }
 
 class Document {
-    constructor(originalName, originlaFileUrl, correctedFileName, correctedFileUrl, state) {
-        this.originalName = originalName
-        this.originlaFileUrl = originlaFileUrl
-        this.correctedFileName = correctedFileName
-        this.correctedFileUrl = correctedFileUrl
-        this.state = state.WAITING
-    }
+  constructor(originalName, originlaFileUrl, correctedFileName, correctedFileUrl, state) {
+    this.originalName = originalName
+    this.originlaFileUrl = originlaFileUrl
+    this.correctedFileName = correctedFileName
+    this.correctedFileUrl = correctedFileUrl
+    this.state = state.WAITING
+  }
 }
 
 class StaticPage {
-    constructor(name, title, content) {
-        this.name = name
-        this.title = title
-        this.content = content
-    }
+  constructor(name, title, content) {
+    this.name = name
+    this.title = title
+    this.content = content
+  }
 }
 
 class Role {
-    constructor(name, permissions) {
-        this.name = name
-        this.permissions = permissions
-    }
+  constructor(name, permissions) {
+    this.name = name
+    this.permissions = permissions
+  }
 }
 
 class Permission {
-    constructor(title) {
-        this.title = title
+  constructor(title) {
+    this.title = title
+  }
+}
+
+
+class GeneralMethods {
+  getRoles() {
+    return {
+      "USER": 1,
+      "TRANSLATOR": 2,
+      "ADMIN": 3
     }
+  }
+
+
+  findUser(searchKey) {
+    const results = []
+    return results
+  }
+
+  getStates() {
+    return {
+      "WAITING": 1,
+      "EXAMINING": 2,
+      "REJECTED": 3,
+      "READY_FOR_DOWNLOAD": 4
+    }
+  }
+
+  getUserInfo(user) {
+    const results = []
+    return results
+  }
 }
 
-role = {
-    "ACADEMIC": 1,
-    "TRANSLATOR": 2,
-    "ADMIN": 3
-}
+const generalMethods = new GeneralMethods()
+const role = generalMethods.getRoles()
+const uygar = new User("uygar", "aydin", "uygar.aydin@erdogan.edu.tr", "23213", "Bilgi İşlem Daire Başkanlığı", "Öğr. Gör.","11", "22", null, role.ADMIN)
 
-state = {
-    "WAITING": 1,
-    "EXAMINING": 2,
-    "REJECTED": 3,
-    "READY_FOR_DOWNLOAD": 4
-}
-
-const uygar = new Academic("uygar","aydin","uygar.aydin@erdogan.edu.tr", "23213", "Bilgi İşlem Daire Başkanlığı", "Öğr. Gör.", role.ACADEMIC, "11", "22")
+console.log(uygar)
