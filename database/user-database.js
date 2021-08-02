77604ebf2ba910b482157147ddc4b61e26118678
uygar aydin <hi@uygaraydin.com>
2021-08-03 02:59:50 +0300

    findUserByIdentificationNumber method was added

--- database/user-database.js
+++ database/user-database.js
@@ -1,19 +1,23 @@
-const BaseDatabase = require('./base-database')
-const User = require('../models/user')
+const BaseDatabase = require("./base-database");
+const User = require("../models/user");
 
 class UserDatabase extends BaseDatabase {
   findUserBy(id) {
-    return this.findBy("id", id)
+    return this.findBy("id", id);
   }
 
   findUser(searchKey) {
-    return this.findBy("name", searchKey)
+    return this.findBy("name", searchKey);
+  }
+
+  findUserByIdentificationNumber(num) {
+    return this.findBy("identificationNumber", num);
   }
 
   getUserInfo(user) {
-    const results = []
-    return results
+    const results = [];
+    return results;
   }
 }
 
-module.exports = new UserDatabase(User)
+module.exports = new UserDatabase(User);
