8188fac754a33309e6d234905d4c26eb21cb70d7
uygar aydin <hi@uygaraydin.com>
2021-08-11 04:27:49 +0300

    unnecessary methods were removed from admin-database and user-database

--- database/admin-database.js
+++ database/admin-database.js
@@ -1,45 +1,8 @@
 const { User } = require("../models");
-const UserDatabase = require("./user-database");
+const { documentDatabase } = require("./");
 
 class AdminDatabase extends UserDatabase {
 
-  delete(user) {
-    return document
-  }
-
-  changeRole(user) {
-    return user
-  }
-
-  edit(staticPage) {
-    return staticPage
-  }
-
-  add(permission) {
-    return permission
-  }
-
-  delete(permission) {
-    return permission
-  }
-
-  updatePermission(permission) {
-    return permission
-  }
-
-  updateRole(role) {
-    return role
-  }
-
-  getStatistics() {
-    // const statistic = new Statistic()
-    // statistic.totalDocument = this.documents.length || 0
-    // statistic.waitingDocument = this.documents.filter(o => o.state == 1).length || 0
-    // statistic.examiningDocument = this.documents.filter(o => o.state == 2).length || 0
-    // statistic.rejectedDocument = this.documents.filter(o => o.state == 3).length || 0
-    // statistic.completedDocument = this.documents.filter(o => o.state == 4).length || 0
-    // return statistic
-  }
 }
 
 module.exports = AdminDatabase(User)
--- database/user-database.js
+++ database/user-database.js
@@ -5,18 +5,6 @@ const getStates = require('../lib/get-states');
 
 class UserDatabase extends BaseDatabase {
 
-  findUserById(id) {
-    return this.findBy("id", id);
-  }
-
-  findUser(searchKey) {
-    return this.findBy("name", searchKey);
-  }
-
-  findUserByIdentificationNumber(num) {
-    return this.findBy("identificationNumber", num);
-  }
-
   login() {
     user = null
     loginServiceResponse = {
@@ -31,15 +19,7 @@ class UserDatabase extends BaseDatabase {
 
   }
 
-  getPersonalStatistics(user) {
-    const statistic = new Statistic()
-    statistic.totalDocument = user.documents.length || 0
-    statistic.waitingDocument = user.documents.filter(o => o.state == getStates().WAITING).length || 0
-    statistic.examiningDocument = user.documents.filter(o => o.state == getStates().EXAMINING).length || 0
-    statistic.rejectedDocument = user.documents.filter(o => o.state == getStates().REJECTED).length || 0
-    statistic.completedDocument = user.documents.filter(o => o.state == getStates().READY_FOR_DOWNLOAD).length || 0
-    return statistic
-  }
+
 
   #controlUserRecord = loginServiceResponse => {
     return this.findUserByIdentificationNumber(loginServiceResponse.identificationNumber)
@@ -48,6 +28,10 @@ class UserDatabase extends BaseDatabase {
   #addFromService = loginServiceResponse => {
     const user = createModelFromObject(loginServiceResponse)
     return userDatabase.insert(user)
+  }
+
+  changeRole(user) {
+    return user
   }
 }
 
